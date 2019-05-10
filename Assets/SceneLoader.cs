5af8f30631614d873705763067ac1e255cb37725
Joshua Yang <ybd1992@hotmail.com>
2019-05-09 17:17:32 -0700

    asset and waypoint system

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    [SerializeField] float delay = 5f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(LoadSceneAfterDelay(delay));
+	}
+
+    IEnumerator LoadSceneAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(0);
+    }
+	
+}
